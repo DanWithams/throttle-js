226946492257054d13915b080e50154a31b35a4f
Daniel Withams <me@danwithams.com>
2024-03-14 16:50:32 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,37 @@
+export class Throttle {
+
+    constructor(ttl) {
+        this.lastTouched = 0;
+        this.ttl = ttl;
+        this. = ttl;
+    }
+
+    static now() {
+        return (new Date).getTime();
+    }
+
+    async touch(fn) {
+        if (this.isThrottled()) {
+            return false;
+        }
+
+        try {
+            await fn.call();
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+
+        this.lastTouched = Throttle.now();
+        return true;
+    }
+
+    isThrottled() {
+        return this.lastTouched + this.ttl > Throttle.now();
+    }
+
+    isNotThrottled() {
+        return ! this.isThrottled();
+    }
+}
+
